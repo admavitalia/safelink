371d9fcb585be1d5a62b4bd4845109f75a804873
admavitalia <38710720+admavitalia@users.noreply.github.com>
2018-04-25 10:36:27 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="https://sl-avitalia.blogspot.com/2018/04/32-ways-to-increase-appetite-for-adults.html"}
-if(x=="2"){xxx="https://sl-avitalia.blogspot.com/2018/04/how-to-treat-vitiligo-disease-causes.html"}
-if(x=="3"){xxx="https://sl-avitalia.blogspot.com/2018/04/causes-of-early-menopause-and-how-to.html"}
-if(x=="4"){xxx="https://sl-avitalia.blogspot.com/2018/04/17-benefits-and-benefits-of-belimbing.html"}
-if(x=="5"){xxx="https://sl-avitalia.blogspot.com/2018/04/12-benefits-of-chia-seed-for-body.html"}a_to_ve[i].href="https://sl-avitalia.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/04/32-ways-to-increase-appetite-for-adults.html"}
+if(x=="2"){xxx="/2018/04/how-to-treat-vitiligo-disease-causes.html"}
+if(x=="3"){xxx="/2018/04/causes-of-early-menopause-and-how-to.html"}
+if(x=="4"){xxx="/2018/04/17-benefits-and-benefits-of-belimbing.html"}
+if(x=="5"){xxx="/2018/04/12-benefits-of-chia-seed-for-body.html"}a_to_ve[i].href="https://sl-avitalia.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
